f64f8bca18ee994fa761f5b0047d7f54e76cb1ab
bomtawep <bomtawep@gmail.com>
2023-08-21 11:11:31 +0700

    Move folder

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 'use client'
 import React, { useEffect, useState } from 'react';
 import { getDocumentTheme, createTheme, NextUIProvider } from '@nextui-org/react'
-import Navigation from '../components/navigation/Navigation'
+import Navigation from '../src/components/navigation/Navigation'
 
 const lightTheme = createTheme({
   type: 'light',
